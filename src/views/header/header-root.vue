35ea46e28380a4ced17c9a694d374906abf17320
Aleksbgbg <aleks.todorov.1337@gmail.com>
2023-01-21 00:47:32 +0000

    Create custom MindArt logo and include as tab icon.

--- src/views/header/header-root.vue
+++ src/views/header/header-root.vue
@@ -5,10 +5,10 @@ import LanguageSelector from "@/views/language/language-selector.vue";
 </script>
 
 <template lang="pug">
-.flex.box-content.max-h-8.px-2.py-3
+.flex.box-content.h-8.px-2.py-3
   div(class="basis-1/6")
   div(class="basis-4/6")
-    logo.mx-auto
+    img.h-8.mx-auto(src="/logo.svg")
   div(class="basis-1/6")
     language-selector.text-sm
 </template>
